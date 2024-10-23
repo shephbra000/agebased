c96d602f2b8605aa0d18d3ecdd8e4a4fb3b912af
shephbra000 <shephbra000@warren.k12.in.us>
2024-10-23 08:20:43 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+age = 14
+if age < 5:
+    print("You should play with toys")
+elif age >= 5 and age <= 12:
+    print("You can play outdoor games")
+    elif age >= 13 and age <= 17:
+    print("You might enjoy video games")
+    if age > 18:
+    print("You can try hiking or traveling")
